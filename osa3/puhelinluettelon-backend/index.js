0a97b976ca63d1c666aa68b70b66600da19a12dd
LaihOsc <oscarlaihoo@gmail.com>
2026-04-09 20:45:02 +0300

    3.15

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -82,8 +82,9 @@ app.post("/api/persons", (request, response) => {
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204).end();
+  Person.findByIdAndDelete(id).then(() => {
+    response.status(204).end();
+  });
 });
 
 const PORT = process.env.PORT;
